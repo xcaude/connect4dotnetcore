7d590908b6a724f6992a763f620abed172360df1
Paul Thibaud <thibaudpaul33@gmail.com>
2025-01-15 23:52:51 +0100

    Blazor Refactor (+ Game page)

--- connect4/obj/Debug/net8.0/connect4.AssemblyInfo.cs
+++ connect4/obj/Debug/net8.0/connect4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("connect4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+018b76ef9c5b500677539a3abf408f33bff6adae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a24752abfef6904ac827651d557479e4ab5a1c35")]
 [assembly: System.Reflection.AssemblyProductAttribute("connect4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("connect4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
